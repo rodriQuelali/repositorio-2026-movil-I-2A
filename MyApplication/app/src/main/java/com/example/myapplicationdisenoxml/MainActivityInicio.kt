bf35e63fa300111c2086be388ff58b3330b92672
rodriquelali <rodriquelali@gmail.com>
2026-04-01 21:49:02 -0400

    Actualización de avance de clase, listo para proyecto.

---
+++ MyApplication/app/src/main/java/com/example/myapplicationdisenoxml/MainActivityInicio.kt
@@ -0,0 +1,30 @@
+package com.example.myapplicationdisenoxml
+
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivityInicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_inicio)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    //funcion que llame a mi otro layout
+    fun llamarActividad(view: View){
+        val i = Intent(this, MainActivity::class.java)
+        startActivity(i)
+    }
+}
