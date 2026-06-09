58ff28422e3ae517afc39eb6c84d007d0444a237
rodriquelali <rodriquelali@gmail.com>
2026-06-09 13:02:24 -0400

    Refactorización de la Clase Calculadora.

--- MyAplicactionuicodeexamplo/app/src/main/java/com/example/myaplicactionui_codeexamplo/data/Calculadora.kt
+++ MyAplicactionuicodeexamplo/app/src/main/java/com/example/myaplicactionui_codeexamplo/data/Calculadora.kt
@@ -1,72 +1,36 @@
 package com.example.myaplicactionui_codeexamplo.data
 
-//Todo, Documentacion
+/**
+* Una calculadora que maneja las operaciones basicas, de la region latinoamericano
+* Formato latinoamericao, normalizacion de forma: 3,45 -> 3.45
+*
+* @property num1 es cadena de texto
+* @property num2 es cadena de texto
+*
+* @constructor creaacion de la intanacia de la clase
+*
+* @author 2do A - 2026
+*
+* @version 1.0
+* */
 
-class Calculadora (var num1: String, var num2: String){
-    //Any depende de lo que se le pase
-    //dos formas
-    //1era es hacer desde constrcutor.
-    //sobrecarga de metodo suma
+class Calculadora (
+    private var num1: String,
+    private var num2: String,
+    private var formatoLatam: FormatoLatam = FormatoLatam()
+    ){
 
-
-    //3,45 -- serapar los digitos
-    //
-
-    //se encontro 2 bug
-    // 1, mi primer bug, solo se coloca una sola coma
-    // 2, segundo bug, 3,1 + 3,9 = 7 entero no 7,0
-
+    /**
+     * Suma de dos numeros
+     * @return suma de de lso dos numeros en forma de cadena de texto*/
     fun suma(): String{
-        var validar = validarDatos()
+        var validar = formatoLatam.validarDatos(num1, num2)
         var n1 = validar[0].toFloat()
         var n2 = validar[1].toFloat()
-        var resulFinal = convertirDatos((n1 + n2).toString())
+        var re = n1 + n2
 
-        return resulFinal.toString()
-        //return this.num1.toString().toFloat() + this.num2.toString().toFloat()
+        return formatoLatam.convertirDatos(re.toString()).toString()
     }
-
-    fun validarDatos(): MutableList<String>{
-        if(this.num1.contains(",") || this.num2.contains(",")){
-            //3,4 +
-            var listDataNu: MutableList<String> = mutableListOf()
-            val nuevoNumero = this.num1.replace(",",".")
-            val nuevoNumero2 = this.num2.replace(",",".")
-
-            var validador = convertirDatosEspecial(nuevoNumero)
-            listDataNu.add(validador.toString())
-
-            validador = convertirDatosEspecial(nuevoNumero2)
-            listDataNu.add(validador.toString())
-
-            return listDataNu
-        }
-        return mutableListOf(this.num1, this.num2)
-    }
-
-    fun convertirDatosEspecial(a:String): Float{
-        return a.toFloat()
-    }
-
-    fun convertirDatos(a:String):Any{
-
-        //soloucionar el bug. de 4 + 5 = 9.0, no se tien que dar eso.
-
-        if(a.toFloat() % 1 == 0f){
-            val aux = a.toFloat()
-            return aux.toInt()
-        }
-        return a.toFloat()
-    }
-
-    //tarea de 09-06-2026, crear una funcion de convertir de . a ,
-    // para elr resultado final, si es float.
-
-
-    //desde los metodos sin el constructor
-    //podemos hacer la sobrecarga de metodos.
-
-
 
 }
 
